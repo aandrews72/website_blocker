8f1ea621cac410b36c97f269459ee600282709b7
aandrews72 <alecandrews2017@gmail.com>
2024-03-12 15:27:42 -0400

    add block functionality with placeholder arg

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ def backup():
     backup = "/etc/hosts.backup"
     if os.path.exists(backup):
         print("A backup file already exists, please check and remove it")
-        return
+        return False
     # backup the host file
     os.system(f"cp /etc/hosts {backup}") 
-    return
+    return True
 
 # Function to unblock websites
 def unblock():
+    backup = "/etc/hosts.backup"
     os.system(f"cp {backup} /etc/hosts")
+    os.remove(backup)
     print("Websites unblocked")
 
-def block(time):
+def block(duration):
     backup()
     # Read the websites from the file
     with open("blocked_websites.txt", "r") as f:
@@ -26,17 +28,24 @@ def block(time):
 
 
     # Block websites by writing contents of blocked_websites.txt to etc/hosts
+    with open("/etc/hosts", "a") as f:
+        for website in blocked_websites:
+            f.write(f"{local} {website}\n")
 
 
     # Calculate the unblock time and print it 
-
+    unblock_time = time.time() + duration * 60
+    # print unblock time with time 
     
-    # While loop to check if we've passed the unblock time 
+    # While loop to check if we've passed the unblock time, if not sleep for 5 seconds
+    while time.time() < unblock_time:
+        time.sleep(5)
 
 
     # Once out of while loop, call the unblock function
-
+    unblock()
     
+block(1)
 
 
 
